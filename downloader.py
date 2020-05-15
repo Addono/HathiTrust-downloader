b781d241959cc6e5d17fa19255f21d387c5c4542
Adriaan Knapen <hi@aknapen.nl>
2020-05-15 16:58:10 +0200

    feat(downloader): change positional arguments to have start and end page

--- downloader.py
+++ downloader.py
@@ -6,13 +6,13 @@ import argparse
 parser = argparse.ArgumentParser(description='Book downloader from Hathitrust')
 
 parser.add_argument('id', type=str, help="The ID of the book, e.g 'mdp.39015027794331'.")
-parser.add_argument('pages', type=int, help="The amount of pages to be downloaded")
-parser.add_argument('--name', dest='name', type=str, help="The start of the filename. Defaults to using the id")
-parser.add_argument('--start-page', dest='start_page', type=int, help="The starting page number", default="1")
+parser.add_argument('start_page', type=int, help="The page number of the first page to be downloaded.")
+parser.add_argument('end_page', type=int, help="The last number of the last page to be downloaded (inclusive).")
+parser.add_argument('--name', dest='name', type=str, help="The start of the filename. Defaults to using the id. This can also be used to change the path.")
 
 args = parser.parse_args()
 
-page_numbers = [i + 1 for i in range(args.start_page, args.pages)]
+page_numbers = [i for i in range(args.start_page, args.end_page + 1)]
 urls = ["https://babel.hathitrust.org/cgi/imgsrv/download/pdf?id=%s;orient=0;size=100;seq=%s;attachment=0" % (args.id, i + 1) for i in page_numbers]
 
 for page_number, url in tqdm(zip(page_numbers, urls), unit="pages", total=len(urls)):
