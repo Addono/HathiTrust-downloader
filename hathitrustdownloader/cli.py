09e140e8bd61459604fcf1d9615e95f34eea10de
Adriaan Knapen <hi@aknapen.nl>
2020-09-14 16:50:10 +0200

    refactor: adds main function as to have a correct entrypoint

--- hathitrustdownloader/cli.py
+++ hathitrustdownloader/cli.py
@@ -4,32 +4,36 @@ import requests
 import time
 import argparse
 
-parser = argparse.ArgumentParser(description='Book downloader from Hathitrust')
+def main():
+    parser = argparse.ArgumentParser(description='Book downloader from Hathitrust')
 
-parser.add_argument('id', type=str, help="The ID of the book, e.g 'mdp.39015027794331'.")
-parser.add_argument('start_page', type=int, help="The page number of the first page to be downloaded.")
-parser.add_argument('end_page', type=int, help="The last number of the last page to be downloaded (inclusive).")
-parser.add_argument('--name', dest='name', type=str, help="The start of the filename. Defaults to using the id. This can also be used to change the path.")
+    parser.add_argument('id', type=str, help="The ID of the book, e.g 'mdp.39015027794331'.")
+    parser.add_argument('start_page', type=int, help="The page number of the first page to be downloaded.")
+    parser.add_argument('end_page', type=int, help="The last number of the last page to be downloaded (inclusive).")
+    parser.add_argument('--name', dest='name', type=str, help="The start of the filename. Defaults to using the id. This can also be used to change the path.")
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-page_numbers = [i for i in range(args.start_page - 1, args.end_page)]
-urls = ["https://babel.hathitrust.org/cgi/imgsrv/download/pdf?id=%s;orient=0;size=100;seq=%s;attachment=0" % (args.id, i + 1) for i in page_numbers]
+    page_numbers = [i for i in range(args.start_page - 1, args.end_page)]
+    urls = ["https://babel.hathitrust.org/cgi/imgsrv/download/pdf?id=%s;orient=0;size=100;seq=%s;attachment=0" % (args.id, i + 1) for i in page_numbers]
 
-for page_number, url in tqdm(zip(page_numbers, urls), unit="pages", total=len(urls)):
-    filename = "%s_p%s.pdf" % (args.name or args.id, str(page_number).zfill(6))
+    for page_number, url in tqdm(zip(page_numbers, urls), unit="pages", total=len(urls)):
+        filename = "%s_p%s.pdf" % (args.name or args.id, str(page_number).zfill(6))
 
-    while True:
-        try:
-            response = requests.get(url, stream=True)
+        while True:
+            try:
+                response = requests.get(url, stream=True)
 
-            if response.ok:
-                with open(filename, "wb") as handle:
-                    for data in response.iter_content():
-                        handle.write(data)
+                if response.ok:
+                    with open(filename, "wb") as handle:
+                        for data in response.iter_content():
+                            handle.write(data)
 
-                break
-        except KeyboardInterrupt:
-            break
-        except:
-            time.sleep(1)
+                    break
+            except KeyboardInterrupt:
+                return
+            except:
+                time.sleep(1)
+
+if __name__ == "__main__":
+    main()
