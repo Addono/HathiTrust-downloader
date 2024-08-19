4e00042c24f9dbbc5531c823528912a0a1435d17
Adriaan Knapen <hi@aknapen.nl>
2024-08-19 16:57:45 +0000

    chore: target up to Python 3.10

--- setup.py
+++ setup.py
@@ -30,6 +30,8 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     keywords='hathitrust download downloader cli',
     packages=find_packages(),
